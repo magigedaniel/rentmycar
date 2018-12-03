0e457056c055d245d1c02e31ec26bc410893f628
Magigedaniel <magigedaniel@gmail.com>
2018-12-03 21:51:23 +0300

    data arrangements

--- resources/views/customer_dashboard.blade.php
+++ resources/views/customer_dashboard.blade.php
@@ -37,11 +37,7 @@
                 <nav class="navbar navbar-default">
                     <div class="container-fluid">
                         <!-- Brand and toggle get grouped for better mobile display -->
-                        <div class="navbar-header">
-                            <button type="button" class="btn btn-primary">All Rents</button>
-                            <button type="button" class="btn btn-primary">Pending Car</button>
-                            <button type="button" class="btn btn-primary">Approved Car</button>
-                        </div>
+                    @include('customer_dashboard_header')
 
                         <!-- Collect the nav links, forms, and other content for toggling -->
                     </div><!-- /.container-fluid -->
