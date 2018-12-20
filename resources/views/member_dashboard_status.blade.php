05c9b9002e43bed7a2459c6aab598afb411bc487
Magigedaniel <magigedaniel@gmail.com>
2018-12-20 17:03:02 +0300

    scroll

--- resources/views/member_dashboard_status.blade.php
+++ resources/views/member_dashboard_status.blade.php
@@ -94,10 +94,10 @@
     <!-- main content end-->
 </section>
 
-<script src="js/jquery.nicescroll.js"></script>
-<script src="js/scripts.js"></script>
+<script src="../js/jquery.nicescroll.js"></script>
+<script src="../js/scripts.js"></script>
 <!-- Bootstrap Core JavaScript -->
-<script src="js/bootstrap.js"></script>
+<script src="../js/bootstrap.js"></script>
 
 </body>
 </html>
